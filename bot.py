aaa3aa8c23b1b285ad806c4d2f7714928a69227f
Mahmoud Bakr <95686919+MahmoudBakr27@users.noreply.github.com>
2023-01-01 17:13:26 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands 
+
+client = commands.Bot(command_prefix="=", intents= discord.Intents.all())
+
+@client.event
+async def on_ready():
+    print ("Success: Bot is connected to Discord")
+
+@client.commands
+async def ping(ctx):
+    await ctx.sends("ping")
+
+
+client.run("#put_the_bot_token_here")    
